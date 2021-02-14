01113692def69d646aacc2608833d086d3243cbc
Shtymak <vova.shtimak@gmail.com>
2021-02-14 23:19:21 +0200

    Toastr notification

--- app/javascript/packs/cart.js
+++ app/javascript/packs/cart.js
@@ -1,12 +1,14 @@
 
 import Rails from "@rails/ujs"
 import * as bootstrap from 'bootstrap'
+import toastr from "toastr";
 
   export function add_to_cart(product_id) {
     Rails.ajax({
       type: "POST",
       url: "/products/" + product_id + "/add_to_cart",
       success: function(response) {
+        toastr.success("Товар доданий до кошика")
         console.log("succses response")
       },
       error: function(response) {
